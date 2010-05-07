affa8f09a2da3cd7fdd4c1fa159dd1da6c3f1100
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2010-05-07 12:12:07 +0200

    wrong character enconding from copy paste

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
-require ‘uri’
+require 'uri'
 
-if ENV[“MONGOHQ_URL”]
-  mongo_uri = URI.parse(ENV[“MONGOHQ_URL”])
-  ENV[‘MONGOID_HOST’] = mongo_uri.host
-  ENV[‘MONGOID_PORT’] = mongo_uri.port.to_s
-  ENV[“MONGOID_USERNAME”] = mongo_uri.user
-  ENV[‘MONGOID_PASSWORD’] = mongo_uri.password
-  ENV[‘MONGOID_DATABASE’] = mongo_uri.path.gsub(“/”, “”)
+if ENV["MONGOHQ_URL"]
+  mongo_uri = URI.parse(ENV["MONGOHQ_URL"])
+  ENV['MONGOID_HOST'] = mongo_uri.host
+  ENV['MONGOID_PORT'] = mongo_uri.port.to_s
+  ENV['MONGOID_USERNAME'] = mongo_uri.user
+  ENV['MONGOID_PASSWORD'] = mongo_uri.password
+  ENV['MONGOID_DATABASE'] = mongo_uri.path.gsub("/", "")
 end
 
 #require 'rails/all'
